fb14d94fa487c18acf2ae7a8e53b58bd0146bc9a
RogerioTeles <53439651+RogerioTeles@users.noreply.github.com>
2020-05-09 17:20:36 -0300

    Nessa versão, foram adicionados os Models (Album, Music e Artist) e foram dados os comandos de migrate  python manage.py makemigrations e python manage.py migrate.

--- ProjetoAV/tune/models.py
+++ ProjetoAV/tune/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+class Musica (models.Model):
+    name = models.CharField('name',max_length = 30 )
+    author = models.CharField('author', max_length = 50)
+    desc = models.CharField('desc', max_length = 500)
+
+
+
+class Album (models.Model):
+    VERSION_CHOICES = (
+        ("Standard","Standard"),
+        ("Deluxe","Deluxe"),
+    )
+    name = models.CharField('name',max_length = 30 )
+    author = models.CharField('author', max_length=50)
+    version = models.CharField(max_length=20, choices=VERSION_CHOICES)
+    desc = models.CharField('desc', max_length=500)
+
+    def __str__(self):
+        return self.name
+
+
+
+class Artist (models.Model):
+    name = models.CharField('name',max_length = 50 )
+    age = models.IntegerField('age')
+    nationality =  models.CharField('name',max_length = 100 )
+
