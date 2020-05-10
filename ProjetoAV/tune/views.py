95bac49ff14dc0cbbee71a2e7f0aa1ff45d75c90
RogerioTeles <53439651+RogerioTeles@users.noreply.github.com>
2020-05-10 02:01:57 -0300

    Layouts das páginas para mostrar Músicas, Artistas e Albuns.

--- ProjetoAV/tune/views.py
+++ ProjetoAV/tune/views.py
@@ -1,20 +1,31 @@
 from django.shortcuts import render
 from tune.models import Album
+from tune.models import Musica
+from tune.models import Artist
 
 # Create your views here.
 
 def index(request):
+
+    return render(request, 'index.html')
+
+def contact(request):
+    listAr = Artist.objects.all()
+    context = {
+        'artistas': listAr
+    }
+    return render(request, 'contact.html',context)
+
+def store(request):
+    listM = Musica.objects.all()
+    context = {
+        'musicas': listM
+    }
+    return render(request, 'store.html', context)
+
+def order(request):
     listA = Album.objects.all()
     context = {
         'albuns': listA
     }
-    return render(request, 'index.html', context)
-
-def contact(request):
-    return render(request, 'contact.html')
-
-def store(request):
-    return render(request, 'store.html')
-
-def order(request):
-    return render(request, 'order.html')
+    return render(request, 'order.html', context)
